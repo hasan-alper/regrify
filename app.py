543a59d82e72cc30d39041f8f25fb559667d63c0
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2022-08-03 18:15:20 +0300

    build a pipeline and improve preprocessing

--- app.py
+++ app.py
@@ -10,22 +10,13 @@ def predict():
     if request.method == "GET":
         return render_template("app.html")
     else:
-        prediction = make_prediction()
-        return render_template("app.html", prediction=prediction)
+        return render_template("app.html", prediction=make_prediction())
 
 
 def make_prediction():
     input = pd.DataFrame(request.form.to_dict(flat=False))
-    temp_input = input.drop(labels="genre", axis=1)
-    temp_input = temp_input.astype(float)
-    genres = pd.DataFrame({"genre_hiphop": [0], "genre_latin": [0], "genre_pop": [0], "genre_r&b": [0], "genre_rap": [0], "genre_rock": [0]})
-    if input["genre"][0] != "genre_edm":
-      genres[input["genre"]] = [1]
-    final_input = pd.concat([temp_input, genres], axis=1)
-    scaler = load("model_assets/scaler.joblib")
-    final_input = scaler.transform(final_input)
     model = load("model_assets/model.joblib")
-    prediction = model.predict(final_input)
+    prediction = model.predict(input)
     return prediction
 
 
